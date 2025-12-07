13f2eef338b0cad9b73e1fb87d921751d5172f15
jgomezz <jgomezz@gmail.com>
2025-12-07 12:45:36 -0500

    first commit

---
+++ src/main/java/pe/edu/tecsup/app/servlets/HelloServlet.java
@@ -0,0 +1,77 @@
+package pe.edu.tecsup.app.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String contenido = 
+				"""
+					<HTML>
+					<HEAD>
+						<meta charset="UTF-8">
+						<title>Insert title here</title>	
+						<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+					</HEAD>
+					<BODY>
+						<div class="container">
+								
+								<div class="page-header">
+									Titulo
+								</div>
+								
+								<div class="page-body">
+						
+								    <button class="btn btn-primary">
+							       		<i class="fa fa-rocket"></i> Bootstrap ready!
+							       </button>
+						
+								</div>
+						              
+								<div class="page-footer">
+									Pie
+								</div>
+								
+							
+						</div>
+						<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script> 
+					</BODY>
+					</HTML>
+				
+				""";
+		
+		
+		response.getWriter().append(contenido);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
