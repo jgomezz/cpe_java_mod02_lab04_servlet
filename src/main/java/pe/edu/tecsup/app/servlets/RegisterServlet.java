002d8953ceb1c60017c1880de630b046132f83da
jgomezz <jgomezz@gmail.com>
2025-12-14 08:38:48 -0500

    update

---
+++ src/main/java/pe/edu/tecsup/app/servlets/RegisterServlet.java
@@ -0,0 +1,47 @@
+package pe.edu.tecsup.app.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, 
+						 HttpServletResponse response) throws ServletException, IOException {
+
+		String username = request.getParameter("username");
+		
+		System.out.println(username);
+		
+		response.getWriter()
+				.append("Bienvenido " + username );
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
